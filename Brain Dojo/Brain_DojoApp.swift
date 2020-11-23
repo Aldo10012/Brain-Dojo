c843be9702c80e53bc6b71e17ac15f00d15b6444
Alberto <aldo10012@gmail.com>
2020-11-22 22:33:28 -0800

    Initial Commit

---
+++ Brain Dojo/Brain_DojoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Brain_DojoApp.swift
+//  Brain Dojo
+//
+//  Created by Alberto Dominguez on 11/22/20.
+//
+
+import SwiftUI
+
+@main
+struct Brain_DojoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
